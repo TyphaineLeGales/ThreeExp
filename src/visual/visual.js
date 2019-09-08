c1d1dc216a6caa32321177152763e8d9d9f7054b
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2019-09-08 22:09:07 +0100

    default

--- src/visual/visual.js
+++ src/visual/visual.js
@@ -8,41 +8,13 @@ exports.update = update;
 
 exports.container = null;
 
-let _material;
-let _geometry;
-let _tex;
-let mesh;
-
 function preInit() {
     exports.container = new THREE.Object3D();
 }
 
 function init () {
-    // exports.container.add(new THREE.Mesh(new THREE.IcosahedronBufferGeometry(0.5, 2), new THREE.MeshNormalMaterial()));
-    _tex = new THREE.TextureLoader().load('matcap2.jpeg');
-    _tex.wrapS = THREE.RepeatWrapping;
-    _tex.wrapT = THREE.RepeatWrapping;
-  _material = new THREE.ShaderMaterial( {
-    uniforms: {
-      u_matCap: {
-        type: 't',
-        value: _tex
-      },
-    },
-    vertexShader: glslify('./visual.vert'),
-    fragmentShader: glslify('./visual.frag'),
-    flatShading: true
-  } );
-  // _material.uniforms.tMatCap.value.wrapS =
-  // _material.uniforms.tMatCap.value.wrapT =
-  // THREE.ClampToEdgeWrapping;
 
-
-  _geometry = new THREE.TorusKnotGeometry( 11, 3, 270, 18, 3, 4);
-
-  mesh = new THREE.Mesh(_geometry, _material);
-
-  exports.container.add(mesh);
+  exports.container.add(new THREE.Mesh(new THREE.IcosahedronBufferGeometry(0.5, 2), new THREE.MeshNormalMaterial()));
 
 }
 
